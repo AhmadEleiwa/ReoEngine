7f253e0412cb8d616892848daf091c7565319dfd
ahmadeleiwa <ahmadilawa@gmail.com>
2024-03-25 18:21:53 +0200

    Working with text rendering and object rendering]

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,12 @@
 
 #include <ui/Text.h>
 #include "core/Window.h"
-#include "core/Program.h"
+#include <core/Camera.h>
+// #include "core/Program.h"
 
-// #include <Windows.h>
 // using namespace std;
 int main()
 {
-
-    // TCHAR buffer[MAX_PATH];
-    // GetCurrentDirectory(MAX_PATH, buffer);
-    // std::cout << buffer << std::endl;
 
     Window *win = new Window(800, 600, "Hello World");
 
@@ -45,15 +41,53 @@ int main()
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-    Program *prog = new Program("vertix_shader.vs", "fragment_shader.fs");
+    Program *prog = new Program("shaders/vertix_shader.vs", "shaders/fragment_shader.fs");
+    Program *prog2 = new Program("shaders/ui_vertix_shader.vs", "shaders/ui_fragment_shader.fs");
 
+    glm::mat4 projection = glm::mat4(1.0f);
+
+    Camera *camera = new Camera();
+    projection = glm::perspective(glm::radians(camera->zoom), (float)win->getWidth() / (float)win->getHeight(), 0.1f, 100.0f);
+    glm::mat4 projection2 = glm::ortho(0.0f, (float)win->getWidth(), 0.0f, (float)win->getHeight());
+    std::string str = "Score: ";
+    Text *text = new Text(str, 0, 0);
+    double pox = 0, poy = 0;
+    float deltaTime, lastTime;
     while (!win->shouldClose())
     {
-        win->clear(1.0f, 0.0f, 0.0f, 1.0f);
+        deltaTime = glfwGetTime() - lastTime;
+        lastTime = glfwGetTime();
+        win->clear(1.0f, 1.0f, 1.0f, 1.0f);
+        // prog2->use();
         prog->use();
+        prog->setMat4("projection", projection);
+        prog->setMat4("view", camera->GetViewMatrix());
+        prog->setMat4("model", glm::mat4(1.0f));
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
+        prog2->use();
+        prog2->setMat4("projection", projection2);
+        text->render(prog2);
+        glfwGetCursorPos(win->getWindow(), &pox, &poy);
+        camera->ProcessMouseMovement(pox, poy, true);
+        if (win->getKeyPressed(GLFW_KEY_S))
+        {
+            camera->ProcessKeyboard(Camera_Movement::BACKWARD, deltaTime);
+        }
+        if (win->getKeyPressed(GLFW_KEY_W))
+        {
+            camera->ProcessKeyboard(Camera_Movement::FORWARD, deltaTime);
+        }
+        if (win->getKeyPressed(GLFW_KEY_A))
+        {
+            camera->ProcessKeyboard(Camera_Movement::LEFT, deltaTime);
+        }
+        if (win->getKeyPressed(GLFW_KEY_D))
+        {
+            camera->ProcessKeyboard(Camera_Movement::RIGHT, deltaTime);
+        }
+
         win->update();
     }
 
